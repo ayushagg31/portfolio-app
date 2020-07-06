fc2c522dfe7c67f61a40582a19eeffed768afd88
Ayush Aggarwal <agg.ayush.1997@gmail.com>
2020-07-06 15:09:09 +0530

    added about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,7 +5,7 @@ import { Container, Row, Col } from 'reactstrap'
 const about = (props) => {
     return (
         <div className={classes.About} id='About'>
-            <Container style={{ height: '50px'}} >
+            <Container style={{ height: '50px' }} >
                 <div></div>
             </Container>
             <Container >
@@ -14,16 +14,19 @@ const about = (props) => {
                 </Row>
                 <Row>
                     <Col className={classes.Content}>
-                        Hi!
-                    <br />
+                        <span role="img" aria-label="hi">👋</span> Hi
                         <br />
-                    Chuck Norris doesn't churn butter. He roundhouse kicks the cows and the butter comes straight out. When the Boogeyman goes to sleep every night, he checks his closet for Chuck Norris CNN was originally created as the "Chuck Norris Network" to update Americans with on-the-spot ass kicking in real-time.
-                    <br />
                         <br />
-                    Chuck Norris was an only child...eventually Chuck Norris is the only man to ever defeat a brick wall in a game of tennis. Chuck Norris invented Kentucky Fried Chicken's famous secret recipe, with eleven herbs and spices. But nobody ever mentions the twelfth ingredient: Fear Chuck Norris has two speeds. Walk, and Kill. Chuck Norris doesn't read books. He stares them down until he gets the information he wants. Chuck Norris does not sleep. He waits. Police label anyone attacking Chuck Norris as a Code 45-11... a suicide Someone once videotaped Chuck Norris getting pissed off. It was called Walker: Texas Chain Saw Massacre. Chuck Norris does not get frostbite. Chuck Norris bites frost. Police label anyone attacking Chuck Norris as a Code 45-11... a suicide.
-                    <br />
+                        I'm Ayush! -  software developer, with a passion for technology and learning. Always enthusiastic by a new idea, and willingness to contribute towards a valuable end product is what pushes me.
                         <br />
-                    Chuck Norris can write a letter by using sign language. Chuck Norris is currently suing NBC, claiming Law and Order are trademarked names for his left and right legs.
+                        <br />
+                        I got introduced to the programming in 11th standard when I opted for computer science as an optional subject, where I end up scoring <i>17/100</i> in my final exam. I failed because things were not making sense. I had too many questions, how come 1+1 is 0, and why <i>a=10</i> works but <i>10=a</i> doesn't, why do we need <i>++a</i> when we have <i>a++</i>. All these things were very complex for a 15-year old child but, after some time everything started to make sense and once I got over the hump I haven't looked back. I got in touch with newer and more modern technologies like Machine Learning, Cloud, Web-development and I just fell in love with programming all over again, after that is still the challenges that get me going.
+                        <br />
+                        <br />
+                        This website is an effort to showcase my fun side projects, skills, that I have developed over the years and continue to do so. You can view the source code of this website&nbsp;<a href="https://github.com/ayushagg31/portfolio-app" rel="noopener noreferrer" target="_blank" style={{ textDecoration: 'none', fontWeight: 'bold', color: '#999999' }}>here</a>, as such, the code is messy and best practices aren't always followed. I am working to make the code more elegant and beautiful. PRs and issue reports are most welcome.
+                        <br />
+                        <br />
+                        If you like what you see, drop a message in the contact section. I would love to hear from you.
                     </Col>
                 </Row>
             </Container>
@@ -32,4 +35,4 @@ const about = (props) => {
 
 }
 
-export default about;
+export default about
