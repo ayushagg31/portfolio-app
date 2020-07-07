043c3e806d33e0dd65fabe1f4cbfe4524b03a18a
Ayush Aggarwal <agg.ayush.1997@gmail.com>
2020-07-07 15:30:38 +0530

    content added

--- src/main/Data.js
+++ src/main/Data.js
@@ -134,7 +134,7 @@ const Projectscroll = {
     C:
     {
         title: 'Audio filtering using ANN',
-        description: `Octave is used to study different characterstics of sound and its related parameters. First, we gave noise and audio(noise+vocals) as an input and retrieve pure vocals after around 50 iterations.`,
+        description: `Octave is used to study different characterstics of sound and its related parameters. First, we gave noise and audio (noise+vocals) as an input and retrieve pure vocals after around 50 iterations.`,
         links:
         {
             github: 'https://github.com/ayushagg31/Audio-clearing-using-machine-learning',
