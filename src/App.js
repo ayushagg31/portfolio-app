ab7217f01efaf2ec014b952774ae80999b11a9fc
Ayush Aggarwal <agg.ayush.1997@gmail.com>
2022-04-24 18:48:46 +0530

    Archived it

--- src/App.js
+++ src/App.js
@@ -1,43 +1,57 @@
-import React, { Component } from 'react'
-import ReactGA from 'react-ga';
-import Navigation from './components/Navigation/Navigation'
-import Intro from './components/Intro/Intro'
-import About from './components/About/About'
-import ExperianceBuilder from './containers/ExperianceBuilder/ExperianceBuilder'
-import EducationBuilder from './containers/EducationBuilder/EducationBuilder'
-import ProjectBuilder from './containers/ProjectBuilder/ProjectBuilder'
-import Skills from './components/Skills/Skills'
-import Contact from './components/Contact/Contact'
-import Footer from './components/Footer/Footer'
-import './preloader.scss'
+import React, { Component } from "react";
+import ReactGA from "react-ga";
+import Navigation from "./components/Navigation/Navigation";
+import Intro from "./components/Intro/Intro";
+import About from "./components/About/About";
+import ExperianceBuilder from "./containers/ExperianceBuilder/ExperianceBuilder";
+import EducationBuilder from "./containers/EducationBuilder/EducationBuilder";
+import ProjectBuilder from "./containers/ProjectBuilder/ProjectBuilder";
+import Skills from "./components/Skills/Skills";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+import "./preloader.scss";
 
 class app extends Component {
   state = {
     loading: true
-  }
+  };
 
   componentDidMount = async () => {
-    ReactGA.initialize('UA-171423142-1');
-    ReactGA.pageview('/')
-    this.loaderTimeOut().then(() => {
-      this.setState({
-        loading: false
-      })
-    })
-  }
+    ReactGA.initialize("UA-171423142-1");
+    ReactGA.pageview("/");
+    // this.loaderTimeOut().then(() => {
+    //   this.setState({
+    //     loading: false
+    //   })
+    // })
+  };
 
   render() {
-    const { loading } = this.state
+    const { loading } = this.state;
     if (loading) {
       return (
-        <div className="rainbow" style={{ color: 'white', fontSize: '1.5rem', textAlign: 'center', paddingTop: '43.4vh' }}>
-          @gozmit #31
+        <div
+          style={{
+            color: "white",
+            fontSize: "1.5rem",
+            textAlign: "center",
+            paddingTop: "43.4vh",
+            fontFamily: "monospace"
+          }}
+        >
+          <strong>
+            {"Rethinking it, old one is "}
+            <a
+              href="https://github.com/ayushagg31/portfolio-app"
+              style={{ cursor: "pointer" }}
+              rel="noopener noreferrer"
+            >
+              archived...
+            </a>
+          </strong>
           <br />
-          <span
-            style={{ color: "Red", fontSize: "1.8rem" }}>
-            <strong>Ayush Aggarwal</strong></span>
         </div>
-      )
+      );
     }
 
     return (
@@ -52,15 +66,14 @@ class app extends Component {
         <Contact />
         <Footer />
       </div>
-    )
+    );
   }
-
 
   loaderTimeOut = () => {
     return new Promise((resolve) => {
-      setTimeout(() => resolve(), 300)
-    })
-  }
+      setTimeout(() => resolve(), 300);
+    });
+  };
 }
 
 export default app;
